1e43419b7f90f4ef78fb34c17412e98d370715d6
Jorge Leon <leon_martins@rede.ulbra.br>
2026-05-30 16:46:32 -0300

    Fechamento oficial das inscrições de C, Lua e TypeScript

--- src/pages/Inscricao.tsx
+++ src/pages/Inscricao.tsx
@@ -26,14 +26,14 @@ type FormData = {
 const COURSES = [
   { name: "Go", disabled: false },
   { name: "Julia", disabled: false },
-  { name: "C", disabled: false },
+  { name: "C", disabled: true },
   { name: "Kotlin", disabled: false },
   { name: "Haskell", disabled: false },
   { name: "Dart", disabled: false },
-  { name: "Lua", disabled: false },
+  { name: "Lua", disabled: true },
   { name: "Rust", disabled: false },
   { name: "Swift", disabled: false },
-  { name: "TypeScript", disabled: false },
+  { name: "TypeScript", disabled: true },
 ];
 
 const Inscricao: React.FC = () => {
