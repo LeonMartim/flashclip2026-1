bd1d809c0fa9a8a34c10672125d70f591abd4ae4
Jorge Leon <leon_martins@rede.ulbra.br>
2026-06-03 10:13:33 -0300

    fechando as demais lingugens

--- src/pages/Inscricao.tsx
+++ src/pages/Inscricao.tsx
@@ -28,10 +28,10 @@ const COURSES = [
   { name: "Julia", disabled: true },
   { name: "C", disabled: true },
   { name: "Kotlin", disabled: true },
-  { name: "Haskell", disabled: false },
-  { name: "Dart", disabled: false },
+  { name: "Haskell", disabled: true },
+  { name: "Dart", disabled: true },
   { name: "Lua", disabled: true },
-  { name: "Rust", disabled: false },
+  { name: "Rust", disabled: true },
   { name: "Swift", disabled: true },
   { name: "TypeScript", disabled: true },
 ];
